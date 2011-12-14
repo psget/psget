483618726f38e5edfbbce8de3d80679a84cf627b
Mike Chaliy <mike@chaliy.name>
2011-12-14 13:58:56 +0200

    Added check if execution policy is restricted to installation script

--- GetPsGet.ps1
+++ GetPsGet.ps1
@@ -3,15 +3,36 @@ function Install-PsGet(){
     new-item ($UserModulePath + "\PsGet\") -ItemType Directory -Force | out-null
     Write-Host Downloading PsGet from https://github.com/chaliy/psget/raw/master/PsGet/PsGet.psm1
     (new-object Net.WebClient).DownloadFile("https://github.com/chaliy/psget/raw/master/PsGet/PsGet.psm1", $UserModulePath + "\PsGet\PsGet.psm1")    
-    import-module PsGet    
+
+    $executionPolicy  = (Get-ExecutionPolicy)
+    $executionRestricted = ($executionPolicy -eq "Restricted")
+    if ($executionRestricted){
+        Write-Warning @"
+Your execution policy is $executionPolicy, this means you will not be able import or use any scripts including modules.
+To fix this change you execution policy to something like RemoteSigned.
+
+        PS> Set-ExecutionPolicy RemoteSigned
+
+For more information execute:
+        
+        PS> Get-Help about_execution_policies
+
+"@
+    }
+
+    if (!$executionRestricted){
+        import-module PsGet
+    }    
     Write-Host "PsGet is installed and ready to use" -Foreground Green
-    Write-Host "USAGE:"
-    Write-Host "    import-module PsGet"
-    Write-Host "    install-module PsUrl"
-    Write-Host ""
-    Write-Host "For more details:"
-    Write-Host "    get-help install-module" 
-    Write-Host "Or visit http://psget.net" 
+    Write-Host @"
+USAGE:
+    PS> import-module PsGet
+    PS> install-module PsUrl
+
+For more details:
+    get-help install-module
+Or visit http://psget.net
+"@
 }
 
 Install-PsGet
